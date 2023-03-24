ff2c2d3d93e0df5be4a7deb3d6221237a973fd57
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-03-24 16:37:29 +0530

    initial commit

--- workspace.js
+++ workspace.js
@@ -1,7 +1,39 @@
-let box = document.querySelector("div");
+/* let box = document.querySelector("div");
     input = document.querySelector("input");
 
 input.addEventListner("input", () => {
     box.style.borderRadius = input.value;
     box.style.background = input.value; 
 });
+ */
+
+document.getElementById("generatePDF").onclick = function{
+    let doc = new jsPDF()
+
+    let users = [
+        {
+            name:'Ragul',
+            age:21,
+            country:'India'
+        },
+        {
+            name:'Sachin',
+            age:15,
+            country:'India'
+        }
+    ]
+
+    let info = []
+
+    users.forEach((element,index,array) => {
+        info.push([element.name,element.age,element.country])
+    });
+
+    doc.autoTable({
+        head:[['Name','Age','Country']],
+        body:info
+
+    })
+
+    doc.save("Profile.pdf")
+}
